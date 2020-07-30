568318f17cb9161dcb01de12113a962b68cfa1c1
egagiilang <62585832+egagiilang@users.noreply.github.com>
2020-07-30 12:31:33 +0700

    Create koneksi.php

---
+++ WEB/koneksi.php
@@ -0,0 +1,8 @@
+<?php  
+	define("host", 'localhost');
+	define("user", 'id14484645_cp');
+	define("pass", "=I<QI1z4ap*]YJGl");
+	define("db", 'id14484645_cartoonp');
+
+	$con = mysqli_connect(host,user,pass,db) or die("Tidak dapat terhubung.");
+?>
