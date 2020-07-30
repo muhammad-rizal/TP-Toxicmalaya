2e464347e5dd3c87ca338574ba015e5b3880438e
egagiilang <62585832+egagiilang@users.noreply.github.com>
2020-07-30 12:31:08 +0700

    Create index.php

---
+++ WEB/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Cartoon Puzzle</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <link rel="stylesheet" href="css/login.css">
+  <link rel="stylesheet" href="background.css">
+  
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+</head>
+<body>
+  <header class="container">
+    <section class="content" style="">
+      <img src="graphic/login/logo.png" style="width: 300px;margin-bottom: 150px;">
+      <form name="formulir" action="mainmenu.php" method="POST" style="margin-bottom: 200px;">
+        <input type="image" src="graphic/login/playbtn.png" alt="Submit button" style="width: 150px;">
+      </form>
+    </section>
+  </header>
+</body>
+</html>
